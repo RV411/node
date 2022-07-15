d5fd452cb83fb747bf66aacfe524472aeb2c7b05
RV411 <ivan_rodriguez3000@hotmail.com>
2022-07-15 15:17:58 -0500

    sequelize model

--- sequilize/src/index.js
+++ sequilize/src/index.js
@@ -1,12 +1,19 @@
 import app from './app.js';
 import { sequelize } from "./database/database.js";
+
+import './models/Project.js'
+import './models/Task.js'
+
 async function main() {
     try {
-        await sequelize.authenticate();
-        console.log("Connection has been established successfully");
+        // await sequelize.authenticate();
+        // console.log("Connection has been established successfully");
+        await sequelize.sync({force:true});
         app.listen(3000);
         console.log("Server is listening on port",3000);
     } catch (error) {
         console.error("Unable to connect to the database:",error)
     }
 }
+
+main();
