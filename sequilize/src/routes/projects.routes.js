edc01ff0e3c1ef6b4f73cfa8703a03c59686f2f1
RV411 <ivan_rodriguez3000@hotmail.com>
2022-07-15 23:55:19 -0500

    CRUD project

---
+++ sequilize/src/routes/projects.routes.js
@@ -0,0 +1,12 @@
+import {Router} from 'express';
+import { getProjects,getProject,createProject,updateProject,deleteProject } from "../controllers/projects-controller.js";
+
+const router=Router();
+
+router.get("/projects",getProjects);
+router.get("/projects/:id",getProject);
+router.post("/projects",createProject);
+router.put("/projects:id",updateProject);
+router.delete("/projects:id",deleteProject);
+
+export default router;
